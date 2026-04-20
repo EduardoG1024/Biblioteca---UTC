c3a4d18d11bf46801070597c773c00450f88f9cb
EduardoG1024 <spawnlol2424@gmail.com>
2026-04-20 10:49:49 -0700

    Supabase Integración

---
+++ README.md
@@ -0,0 +1,23 @@
+# REGISTRO DE ESTUDIANTES
+
+- Aplicacion web para registro de estudiantes de la universidad
+- La aplicacion permite registrar a varios estudiantes al mismo tiempo y guardar su entrada y salida en un solo lugar.
+- La aplicacion permite generar reportes automaticos diarios o generar reportes manualmente.
+
+![Alt img](/assets/frontend.png)
+
+## ¿Por que utilizar esta herramienta? 
+
+- Para ya no usar hojas que despues se tiran
+
+## Desarrollo
+
+- Esta aplicacion se creo utilizando distintas herramientas como: NodeJs, Supabase, HTML & CSS
+- HTML & CSS: 
+    - Estos lenguajes se utilizan para desarrollar el frontend para que los estudiantes puedan ingresar sus datos y enviarlos al backend.
+- NodeJs:
+    - NodeJs se utiliza para el backend, NodeJs recibe los datos enviados desde el fronend y con middlewares/filtros almacenar los datos en supabase.
+- Supabase:
+    - Supabase es una base de datos que se utiliza para almacenar los datos del backend, Supabase guarda los datos de manera ordenada segun el tipo de dato ingresado.
+
+![alt img](/assets/supabase.png)
--- main.js
+++ main.js
@@ -5,6 +5,11 @@ import rateLimit from "express-rate-limit";
 import session from "express-session";
 import { createClient } from "@supabase/supabase-js";
 
+const supabase = createClient(
+    process.env.SUPABASE_PUBLIC_URL,
+    process.env.SUPABASE_ANON_KEY
+);
+
 const port = process.env.PORT;
 const app = express();
 
@@ -13,14 +18,39 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
 
+// LIMITADOR MIDDLEWARE
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 Minutos
+	limit: 5,
+	standardHeaders: 'draft-8',
+	legacyHeaders: false,
+    message: 'Ya no Puedes Enviar mas Formularios'
+});
+
 // REDIRECCIONAMIENTO
 app.get('/', (req, res) => {
-    res.redirect('index.html')
-})
+    res.redirect('index.html');
+});
 
-// ENDPOINT DATOS USUARIOS
-app.post('/registro', (req, res) => {
+// ENDPOINT DATOS USUARIOS & SUPABASE
+app.post('/registro', limiter, async (req, res) => {
     console.log(req.body);
+    try {
+        const { data, error} = await supabase
+        .from('Biblioteca UTC Registros')
+        .insert({
+            Estudiante: req.body.Estudiante,
+            Matricula: req.body.Matricula,
+            Carrera: req.body.Carrera,
+            Fecha: req.body.Dia,
+            Hora_Entrada: req.body.HoraEntrada,
+            Hora_Salida: req.body.HoraSalida,
+            Asunto: req.body.Asunto 
+
+        });
+    } catch (error) {
+        res.status(500).send('Error al Guardar Registro');
+    };
     res.send('Datos Guardados');
 });
 
